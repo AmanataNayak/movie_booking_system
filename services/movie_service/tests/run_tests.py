51afebdbc5356343924b8542c9b160f11133301e
Amanata Nayak <amanatanayak@gmail.com>
2025-09-23 23:16:27 +0530

    update bazel

---
+++ services/movie_service/tests/run_tests.py
@@ -0,0 +1,7 @@
+import sys
+import pytest
+
+# This command explicitly starts pytest and tells it to discover
+# and run all tests in the current directory and subdirectories.
+if __name__ == "__main__":
+    sys.exit(pytest.main())
